3d996bf51eff309dded8114012b446a458e76ddb
ProjetoFinalULP <a21504083@mso365.ulp.pt>
2019-05-24 18:57:27 +0100

    Start

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include 'config.inc';
+
+$sql = "SELECT description, userCreation From weekDay";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["description"]. " - Name: " . $row["userCreation"]. " <br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>		
