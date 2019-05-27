cce0b3f7ca7ed6fb5369e1f643e91432d81fe591
ProjetoFinalULP <a21504083@mso365.ulp.pt>
2019-05-27 18:55:31 +0100

    awdaw

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pl">
   <head>
-    <title>Page title</title>
+    <title>Go Up Fitness</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="pl">
   <head>
 
-    <title>Page title</title>
+    <title>Go Up Fitness</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
